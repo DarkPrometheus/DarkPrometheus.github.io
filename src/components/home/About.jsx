cfeb61eb85266c9d02490342a563d47e92e6360b
EmmanuelFrias <emmanuelfriasdev@gmail.com>
2023-07-18 12:39:14 -0700

    Actualizacion de About

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -5,11 +5,9 @@ export const About = () => {
         <div id="#about" className="About__container">
             <h3>Acerca de mi</h3>
             <p>
-                Soy estudiante de ingeniería Informatica. Disfruto trabajando y
-                aprendiendo en mi computadora. Me encanta el mundo del hardware
-                y software. Me gusta mucho la plataforma de .NET y el desarrollo
-                web, ademas de que son las dos tecnologías que más domino. En el
-                futuro estudiare para convertirme en un Data Engineer
+                Soy un ingeniero informático especializado en .NET con 2 años de experiencia y 1 año trabajando con la plataforma .NET. Como estudiante de ingeniería informática, disfruté aprendiendo y trabajando en mi computadora, explorando tanto los aspectos de hardware como de software.
+                A lo largo de estos años, he adquirido conocimientos en tecnologías relacionadas con .NET y desarrollo web. Me siento cómodo trabajando con frameworks como ASP.NET, .NET Core, C y SQL Server a nivel básico.
+                En el futuro estudiare para convertirme en un Data Engineer
             </p>
         </div>
     );
